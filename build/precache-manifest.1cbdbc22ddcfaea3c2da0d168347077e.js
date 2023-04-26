2c4b8d87206deba31412eacb65a24cb806a317fc
ruti5232 <ruti5232@gmail.com>
2023-04-26 23:37:45 +0300

    second commit

---
+++ build/precache-manifest.1cbdbc22ddcfaea3c2da0d168347077e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "952f58a14e410df71b289535cd72be25",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a19ad8931ce4edacd7d1",
+    "url": "/static/js/2.0fc097c4.chunk.js"
+  },
+  {
+    "revision": "abf6fcace3adbf8ab345683d164de87d",
+    "url": "/static/js/2.0fc097c4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2a65b94fdab7bfaab1ce",
+    "url": "/static/js/main.dd911513.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/static/js/main.dd911513.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ed24628c41b89e511d36",
+    "url": "/static/js/runtime-main.76e1faf0.js"
+  }
+]);
